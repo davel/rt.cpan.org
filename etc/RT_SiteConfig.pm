12c5857010a0f15f162a48bb6ab0e4fefe7132d7
Dave Lambley <dave@lambley.me.uk>
2021-02-20 22:43:43 +0000

    Log to stderr

--- etc/RT_SiteConfig.pm
+++ etc/RT_SiteConfig.pm
@@ -13,6 +13,7 @@ Set($DatabaseName, 'rt_cpan_org');
 Set( $LogToSyslog, undef );
 Set( $LogToScreen, $ENV{RT_LOG_LEVEL} || ($DEV ? "info" : undef) );
 Set( $LogToFile, "warn" );
+Set( $LogToFileNamed, "/dev/stderr");
 Set( $LogStackTraces, "crit" );
 
 Set( $WebDomain, $hostname);
